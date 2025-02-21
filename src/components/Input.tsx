baf47a92737b94b6bdf005404db138b6d433df43
Guilherme <150380352+guioliveirx@users.noreply.github.com>
2025-02-21 15:58:35 -0300

    refactor: simplify Input component by removing unnecessary props and structure

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,24 +1,4 @@
-import { Mail } from 'lucide-react'
 import React, { type ComponentProps } from 'react'
-
-interface InputProps extends ComponentProps<'input'> {
-  error?: boolean
-}
-
-const Input = ({ error = false, ...props }: InputProps) => {
-  return (
-    <div
-      data-error={error}
-      className="data-[error=true]:border-danger group flex items-center gap-2 bg-gray-800 border border-gray-600 w-full h-12 px-4 rounded-xl focus-within:border-gray-100"
-    >
-      <span className="text-gray-400 group-focus-within:text-gray-100 group-[&:not(:has(input:placeholder-shown))]:text-gray-100 group-data-[error=true]:text-danger">
-        <Mail />
-      </span>
-
-      <input className="flex-1 outline-0 placeholder-gray-400" {...props} />
-    </div>
-  )
-}
 
 interface InputRootProps extends ComponentProps<'div'> {
   error?: boolean
